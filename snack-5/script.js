23763ad0242ffe5ff0fde5474fa07da7a47f9ad1
matteo-giustini <matteo.giustini3@gmail.com>
2024-12-17 12:59:32 +0100

    feat: add snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,5 @@
 const nums = [2, 8, 4, 7, 12, 87];
-
+const even = nums.filter(elm => elm % 2 === 0)
+console.log(even)
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
