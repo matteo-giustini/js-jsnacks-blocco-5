a9d03a9e4ff6d460807b2ad9829616e7ea8ff551
matteo-giustini <matteo.giustini3@gmail.com>
2024-12-17 12:42:43 +0100

    feat: add snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,6 +3,8 @@ const people = [
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
+const names = people.map(elm => elm.name);
+names.forEach(elm => console.log(elm))
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
